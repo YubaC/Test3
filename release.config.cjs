9fdba42a088d9a255feb5533f15fc1e3eb953343
YubaC <49724342+YubaC@users.noreply.github.com>
2025-02-14 22:53:21 +0800

    ✨ Feat(core)!: Shitting regex

--- release.config.cjs
+++ release.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
                     { breaking: true, release: "major" },
                     { type: "Feat", release: "minor" },
                 ],
+                debug: true,
             },
         ],
 
