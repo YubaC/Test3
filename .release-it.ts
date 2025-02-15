69f82334b0aa9b20cb7b6a320a01d42c2fbe17a2
YubaC <49724342+YubaC@users.noreply.github.com>
2025-02-15 13:31:39 +0800

    ✨ feat(release): 更新提交头部模式以支持破坏性更改标记

--- .release-it.ts
+++ .release-it.ts
@@ -21,7 +21,7 @@ export default {
             options: {
                 parserOpts: {
                     //         // 匹配可选的 emoji（Unicode 范围可能需根据情况调整），后面跟空格，再匹配大写 type、可选 scope、冒号、空格和 subject
-                    headerPattern: /^.?\s?([A-Za-z]+)(?:\((.*)\))?(!?): (.*)$/,
+                    headerPattern: /^.?\s?([A-Za-z]+)(?:\((.*)\))?: (.*)$/,
                     //         headerCorrespondence: [
                     //             "type",
                     //             "scope",
